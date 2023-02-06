7da2edf43c23c5e967fa1505a70c0438cdefec18
Nikita <khakimov.nikita.vch@gmail.com>
2023-02-06 20:40:38 +0200

    nestedQueries

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,4 +1,6 @@
 import Query from './query.js';
 import Mutation from './mutation.js';
+import Note from './note.js';
+import User from './user.js';
 
-export { Query, Mutation };
+export { Query, Mutation, Note, User };
